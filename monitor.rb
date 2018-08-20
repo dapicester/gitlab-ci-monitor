4fbee3579f4618b236cd96579c21550a88c103d2
Paolo D'Apice <paolo@sidechef.com>
2018-08-20 12:26:54 +0800

    Fetch pipeline details in a separate API call, new in v4

--- monitor.rb
+++ monitor.rb
@@ -44,39 +44,51 @@ class BuildFetcher
   class NetworkError < StandardError; end
 
   def initialize(project_id, api_token, logger: DummyLogger.new)
+    @project_id = project_id
     @api_token = api_token
     @logger = logger
-    @uri = URI "#{BASE_URI}/projects/#{project_id}/pipelines"
   end
 
   def latest_build(branch = 'develop')
     @logger.info { 'Fetching pipelines ...' }
 
-    response = Net::HTTP.start(@uri.host, @uri.port, use_ssl: true) do |http|
-      request = Net::HTTP::Get.new @uri
-      request.add_field 'PRIVATE-TOKEN', @api_token
-
-      http.request request
-    end
-
-    @logger.debug { response }
-
-    if response.code.to_i != 200
-      @logger.debug { response.body.inspect.light_yellow }
-      message = "#{response.message.red} (#{response.code.red}): #{response.body.underline}"
-      raise ServerError, message
-    end
-
+    pipelines_url = "#{BASE_URI}/projects/#{@project_id}/pipelines"
+    response = fetch pipelines_url
     pipelines = JSON.parse response.body, symbolize_names: true
 
     # returned build are already sorted
-    last_build = pipelines.find { |el| el[:ref] == branch }
+    latest = pipelines.find { |el| el[:ref] == branch }
+
+    detail_url = "#{pipelines_url}/#{latest[:id]}"
+    response = fetch detail_url
+    last_build = JSON.parse response.body, symbolize_names: true
     @logger.debug { "Last build on #{branch}: #{last_build.inspect.light_yellow}" }
 
     last_build
   rescue SocketError, Timeout::Error => ex
     raise NetworkError, ex
   end
+
+  private
+
+    def fetch(url)
+      uri = URI url
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        request = Net::HTTP::Get.new uri
+        request.add_field 'PRIVATE-TOKEN', @api_token
+
+        http.request request
+      end
+      @logger.debug { response }
+
+      if response.code.to_i != 200
+        @logger.debug { response.body.inspect.light_yellow }
+        message = "#{response.message.red} (#{response.code.red}): #{response.body.underline}"
+        raise ServerError, message
+      end
+
+      response
+    end
 end
 
 # Controls LEDs on an Arduino board with Firmata.
--- test/gitlab_client_test.rb
+++ test/gitlab_client_test.rb
@@ -10,23 +10,50 @@ class BuildFetcherTest < Minitest::Test
   def test_latest_build
     pipelines = [
       {
-        'id': 47,
-        'status': 'pending',
-        'ref': 'master',
-        'sha': 'a91957a858320c0e17f3a0eca7cfacbff50ea29a'
+        id: 47,
+        status: 'pending',
+        ref: 'master',
+        sha: 'a91957a858320c0e17f3a0eca7cfacbff50ea29a'
       },
       {
-        'id': 48,
-        'status': 'pending',
-        'ref': 'develop',
-        'sha': 'eb94b618fb5865b26e80fdd8ae531b7a63ad851a'
+        id: 48,
+        status: 'pending',
+        ref: 'develop',
+        sha: 'eb94b618fb5865b26e80fdd8ae531b7a63ad851a'
       }
     ]
-
     stub_request(:get, @url).to_return(status: 200, body: pipelines.to_json)
 
+    detail = {
+      id: 48,
+      status: 'pending',
+      ref: 'develop',
+      sha: 'a91957a858320c0e17f3a0eca7cfacbff50ea29a',
+      before_sha: 'a91957a858320c0e17f3a0eca7cfacbff50ea29a',
+      tag: false,
+      yaml_errors: nil,
+      user: {
+        name: 'Administrator',
+        username: 'root',
+        id: 1,
+        state: 'active',
+        avatar_url: 'http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+        web_url: 'http://localhost:3000/root'
+      },
+      created_at: '2016-08-11T11:28:34.085Z',
+      updated_at: '2016-08-11T11:32:35.169Z',
+      started_at: nil,
+      finished_at: '2016-08-11T11:32:35.145Z',
+      committed_at: nil,
+      duration: nil,
+      coverage: '30.0',
+      web_url: 'https://example.com/foo/bar/pipelines/46'
+    }
+    detail_url = "#{@url}/#{detail[:id]}"
+    stub_request(:get, detail_url).to_return(status: 200, body: detail.to_json)
+
     build = @subject.latest_build
-    assert_equal pipelines.last, build
+    assert_equal detail, build
   end
 
   def test_latest_build_error
